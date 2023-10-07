57293b33413c7148fa02d7cf7b3885aaa4b36255
Dianne <djs.dianne@gmail.com>
2023-10-07 05:33:20 -0400

    refined layout spacing and image positioning

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -5,19 +5,19 @@ function HeroImage() {
   return (
     <section className="">
       <div className="card border-0 bg-transparent w-75">
-        <div className="card-body">
-          <h2 className="card-title fs-2 lh-1">
+        <div className="card-body p-0">
+          <h2 className="card-title fs-2 lh-1 mb-2">
             Hello, I'm Dianne M. De Jesús a{" "}
             <span className="fs-1 fw-bolder title-color">
               {"{ Software Developer }"}
             </span>
           </h2>
-          <p className="card-text">
+          <p className="card-text m-0">
             Who is currently building projects that serve my current clients and
             my own personal curiosities and interests.
           </p>
           <div className="mt-5">
-            <a href="#" className="fs-3 me-4 py-3 px-5 btn button-theme">
+            <a href="#" className="fs-3 me-2 py-3 px-5 btn button-theme">
               Hire Me
             </a>
 
