d6c229c9c26b6c7357ee4d5b8993ba6d2ca8aa4a
Dianne <djs.dianne@gmail.com>
2023-11-16 20:11:40 -0400

    test deploys

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/portfolio2/',
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
 })
